5abeb5936fe49de43b5a07a1906d02fa12a74119
Dark_Benky <matus.benky@gmail.com>
2024-04-04 00:28:01 +0200

    Delete unused map files

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import random
 import os
 import numpy as np
 import json
+import sys
 # import time
-from PIL import Image, ImageDraw
+# from PIL import Image, ImageDraw
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
+import math
+import pygame
 # from numba import jit
 
 INVALID_MOVE_PENALTY = -5
 PREFORMED_MOVE_PENALTY = -0.5
+STACKED_BOX = -10
 WIN_REWARD = 100
 BOX_PUSH_REWARD = 1.5
 BOX_CLOSE_TO_GOAL_REWARD = 2.5
@@ -84,31 +89,29 @@ class SokobanEnv:
 
         return arena
     
-    # @staticmethod
-    # @jit(nopython=True)
-    def create_observation(self):
-        obs = np.zeros((self.arena.shape[0], self.arena.shape[1], 5), dtype=np.int8)
-    
-        # Encode player position
-        obs[self.playerXY[0], self.playerXY[1], 0] = 1
-        
-        # Encode box positions
+    def box_is_movable(self,):
+        corner_positions = [(0, 0), (9, 9), (0, 9), (9, 0)]
+        if np.any(np.all(np.isin(self.boxes, corner_positions), axis=1)):
+            return False
         for box in self.boxes:
-            obs[box[0], box[1], 1] = 1
-        
-        # Encode goal positions
-        for goal in self.goals:
-            obs[goal[0], goal[1], 2] = 1
-            
-        for barrier in self.barriers:
-            obs[barrier[0], barrier[1], 3] = 1
-            
-        valid_moves = self.valid_moves()
-        for move in valid_moves:
-            dx , dy = self.get_direction_to_dx_dy(move)
-            obs[self.playerXY[0] + dx , self.playerXY[1] + dy , 4] = 1
-            
-        return obs
+            corner_list = [ ((1,0),(0,1)), ((1,0),(0,-1)),
+                           ((-1,0),(0,1)), ((-1,0),(0,-1)) ]
+
+            for corner in corner_list:
+                corner1_dx, corner1_dy = corner[0]
+                corner2_dx, corner2_dy = corner[1]
+                corner1_x, corner1_y = box[0] + corner1_dx, box[1] + corner1_dy
+                corner2_x, corner2_y = box[0] + corner2_dx, box[1] + corner2_dy
+                
+                if (corner1_x >= 0 and corner1_x < 10) and (corner1_y >= 0 and corner1_y < 10) and (corner2_x >= 0 and corner2_x < 10) and (corner2_y >= 0 and corner2_y < 10):
+                    try:    
+                        if self.arena[corner1_x][corner1_y] == 1 and self.arena[corner2_x][corner2_y] == 1:
+                            return False
+                    except IndexError:
+                        return False
+                else:
+                    return False
+        return True
     
     # @staticmethod
     # @jit(nopython=True)
@@ -121,6 +124,18 @@ class SokobanEnv:
             return 0, -1
         elif direction == 'D':
             return 0, 1
+    
+        
+    def is_box_close_to_goal(self):
+        for box in self.boxes:
+            for goal in self.goals:
+                if np.linalg.norm(box - goal) == 1:
+                    # Check if there's an empty space next to the goal
+                    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                        next_pos = goal + np.array([dx, dy])
+                        if np.all(next_pos == box):
+                            return True
+        return False
 
     # @staticmethod
     # @jit(nopython=True)
@@ -228,36 +243,66 @@ class SokobanEnv:
                     return True
         return False
     
+    def reverse_direction(self, direction):
+        if direction == 'W':
+            return 'S'
+        elif direction == 'S':
+            return 'W'
+        elif direction == 'A':
+            return 'D'
+        elif direction == 'D':
+            return 'A'
+    
     def step(self, action):
         # Take a step in the environment based on the selected action
         direction = self.get_direction_from_action(action)
-        
-        box_positions = self.boxes.copy()
-        
-        previous_player_position = self.playerXY.copy()
-        
-        success = self.move(direction)
 
-        reward = 0
-        done = False
+        checkpoint = self.is_box_close_to_goal()
 
-        if success:
-            self.arena = self.construct_arena()
-            if self.is_win():
-                reward = WIN_REWARD
-                done = True
+        if checkpoint:
+            if self.move(direction):
+                if self.is_win():
+                    return self.reset() , WIN_REWARD , True, {}
+                else:
+                    if self.is_box_close_to_goal():
+                        self.arena = self.construct_arena()
+                        return self.arena , BOX_CLOSE_TO_GOAL_REWARD, False, {}
+                    else:
+                        direction = self.reverse_direction(direction)
+                        self.move(direction)
+                        self.arena = self.construct_arena()
+                        return self.arena , BOX_CLOSE_TO_GOAL_REWARD, False, {}
             else:
-                reward += PREFORMED_MOVE_PENALTY
-                if np.any(self.boxes != box_positions):
-                    reward += BOX_PUSH_REWARD
-                    if self.box_closer_to_goal(box_positions , self.boxes):
-                        reward += BOX_CLOSE_TO_GOAL_REWARD
-                if self.player_closer_to_box(previous_player_position , self.playerXY):
-                    reward += PLAYER_CLOSE_TO_BOX_REWARD
+                return self.arena, INVALID_MOVE_PENALTY, False, {}
         else:
-            reward = INVALID_MOVE_PENALTY
+            box_positions = self.boxes.copy()
+            previous_player_position = self.playerXY.copy()
+            success = self.move(direction)
 
-        return self.arena, reward, done, {}
+            done = False
+            reward = 0
+
+            if success:
+                self.arena = self.construct_arena()
+                if self.is_win():
+                    reward = WIN_REWARD
+                    print('WIN')
+                    done = True
+                else:
+                    if self.box_is_movable() == False:
+                        return self.reset(), STACKED_BOX, True, {}
+
+                    reward += PREFORMED_MOVE_PENALTY
+                    if np.any(self.boxes != box_positions):
+                        reward += BOX_PUSH_REWARD
+                        if self.box_closer_to_goal(box_positions , self.boxes):
+                            reward += BOX_CLOSE_TO_GOAL_REWARD
+                    if self.player_closer_to_box(previous_player_position , self.playerXY):
+                        reward += PLAYER_CLOSE_TO_BOX_REWARD
+            else:
+                reward = INVALID_MOVE_PENALTY
+
+            return self.arena, reward, done, {}
     
     def reset(self):
         barriers, player_x, player_y, box, goals = load_function_from_json('maps')
@@ -293,9 +338,10 @@ class DQN(nn.Module):
     def __init__(self, input_dim, output_dim):
         super(DQN, self).__init__()
         self.fc1 = nn.Linear(input_dim, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 128)
-        self.fc4 = nn.Linear(128, output_dim)  # Output dimension matches the number of actions
+        self.fc2 = nn.Linear(128, 256)
+        self.fc3 = nn.Linear(256, 256)
+        self.fc4 = nn.Linear(256, 128)
+        self.fc5 = nn.Linear(128, output_dim)  # Output dimension matches the number of actions
 
     def forward(self, x):
         # Flatten the input tensor to be compatible with fully connected layers
@@ -303,12 +349,13 @@ class DQN(nn.Module):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = torch.relu(self.fc4(x))
+        x = self.fc5(x)
         return x
 
 
 class DQNAgent:
-    def __init__(self, env, replay_capacity=10000, batch_size=32):
+    def __init__(self, env, replay_capacity=10000, batch_size=32, epsilon_start=1.0, epsilon_end=0.01, epsilon_decay=0.000_000_1 , strategy='linear'):
         self.env = env
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.policy_net = DQN(np.prod(env.arena.shape), len(env.action_space)).to(self.device)
@@ -319,19 +366,54 @@ class DQNAgent:
         self.loss_fn = nn.MSELoss()
 
         self.discount_factor = 0.99
-        self.epsilon = 0.05
+        self.epsilon = epsilon_start
+        self.epsilon_start = epsilon_start
+        self.epsilon_end = epsilon_end
+        self.epsilon_decay = epsilon_decay
+        self.step_number = 0
+        
+        self.strategy = strategy
 
         self.replay_memory = ReplayMemory(replay_capacity)
         self.batch_size = batch_size
 
     def choose_action(self, state):
-        if np.random.rand() < self.epsilon:
-            return np.random.choice(self.env.action_space)
+        if self.strategy == 'linear':
+            self.epsilon = max(self.epsilon_end, self.epsilon - self.epsilon_decay)
+        elif self.strategy == 'exponential':
+            self.epsilon = self.epsilon_start * math.exp(-1 * self.epsilon_decay * self.step_number)
+        elif self.strategy == 'constant':
+            self.epsilon = self.epsilon_end
+        elif self.strategy == 'epsilon_greedy':
+            self.epsilon = max(self.epsilon_end, self.epsilon_start - self.epsilon_decay * self.step_number)
+            if np.random.rand() < self.epsilon:
+                return np.random.choice(self.env.action_space)
+        elif self.strategy == 'boltzmann':
+            if self.epsilon_decay < 0:
+                raise ValueError("Temperature (epsilon_decay) must be positive for Boltzmann exploration")
+            q_values = self.policy_net(torch.tensor(state, dtype=torch.float32).to(self.device))
+            boltzmann_probs = F.softmax(q_values / self.epsilon_decay, dim=-1)
+            action = torch.multinomial(boltzmann_probs, 1)
+            return action.item()
+        elif self.strategy == 'softmax':
+            if self.epsilon_decay < 0:
+                raise ValueError("Temperature (epsilon_decay) must be positive for Softmax exploration")
+            q_values = self.policy_net(torch.tensor(state, dtype=torch.float32).to(self.device))
+            softmax_probs = F.softmax(q_values / self.epsilon_decay, dim=-1)
+            action = torch.multinomial(softmax_probs, 1)
+            return action.item()
         else:
-            with torch.no_grad():
-                state_tensor = torch.tensor(state, dtype=torch.float32).to(self.device)
-                state_tensor = state_tensor.unsqueeze(0)  # Add batch dimension
-                q_values = self.policy_net(state_tensor)
+            raise ValueError('Invalid strategy')
+        
+        self.step_number += 1
+        
+        with torch.no_grad():
+            state_tensor = torch.tensor(state, dtype=torch.float32).to(self.device)
+            state_tensor = state_tensor.unsqueeze(0)  # Add batch dimension
+            q_values = self.policy_net(state_tensor)
+            if np.random.rand() < self.epsilon:
+                return np.random.choice(self.env.action_space)
+            else:
                 return q_values.argmax().item()  # Return the action with the highest Q-value
 
     def learn(self):
@@ -363,6 +445,11 @@ class DQNAgent:
     def train(self, num_episodes, reset_threshold=250_000):
         best_average_reward = -float('inf')
         best_episode = 0
+        
+        pygame.init()
+        screen_width, screen_height = 800, 800
+        screen = pygame.display.set_mode((screen_width, screen_height))
+
 
         for episode in range(num_episodes):
             state = self.env.reset()
@@ -372,6 +459,11 @@ class DQNAgent:
             verbose = 1_000
 
             while not done:
+                
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        sys.exit()
+                
                 action = self.choose_action(state)
                 next_state, reward, done, _ = self.env.step(action)
                 self.replay_memory.push(Transition(state, action, reward, next_state, done))
@@ -391,12 +483,42 @@ class DQNAgent:
                 # Log progress
                 if current_step % verbose == 0 and current_step > 0:
                     print(f"Current step: {current_step}, Total reward: {total_reward}, Average reward: {total_reward / current_step}")
+                    print(f"Epsilon : {self.epsilon}")
+
+                """ arena is 10*10 grid
+                player == 4
+                box == 3 
+                goal == 2
+                barrier == 1
+                empty space == 0
+                """
+                arena = self.env.arena
+                
+                # render this arena
+                block_size = screen_width // 10
+                
+                for i in range(10):
+                    for j in range(10):
+                        if arena[i][j] == 4:
+                            pygame.draw.rect(screen, (255, 0, 0), (block_size * i, block_size * j, block_size, block_size))
+                        elif arena[i][j] == 3:
+                            pygame.draw.rect(screen, (0, 128, 0), (block_size * i, block_size * j, block_size, block_size))
+                        elif arena[i][j] == 2:
+                            pygame.draw.rect(screen, (0, 0, 255), (block_size * i, block_size * j, block_size, block_size))
+                        elif arena[i][j] == 1:
+                            pygame.draw.rect(screen, (255, 255, 255), (block_size * i, block_size * j, block_size, block_size))
+                        else:
+                            pygame.draw.rect(screen, (0, 0, 0), (block_size * i, block_size * j, block_size, block_size))
+                            
+                pygame.display.flip()  # Update display
+
 
             # Update target network periodically
             if episode % TARGET_UPDATE == 0:
                 self.target_net.load_state_dict(self.policy_net.state_dict())
 
             # Print episode information
+            print(f'Epsilon :{self.epsilon}')
             print(f"Episode {episode+1}/{num_episodes}, Total Reward: {total_reward}, Average Reward: {total_reward / current_step}")
 
         print(f"Best average reward achieved: {best_average_reward} at episode {best_episode}")
@@ -413,6 +535,6 @@ class DQNAgent:
 barriers, player_x, player_y, box, goals = load_function_from_json('maps')
 env = SokobanEnv(playerXY=(player_x, player_y) , barriers=barriers , boxes=box , goals=goals)  # Instantiate environment
 env.reset()
-agent = DQNAgent(env, replay_capacity=100_000 , batch_size=48)  # Instantiate agent
+agent = DQNAgent(env, replay_capacity=10_000_000 , batch_size=10_000)  # Instantiate agent
 agent.train(num_episodes=1000)  # Train the agent
-agent.save_q_table('q_table.npy')  # Save the NN weights to a file
+agent.save_q_table('DQN')  # Save the NN weights to a file
